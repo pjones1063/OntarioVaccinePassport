611e7dd84ef079a06fe16fcda9b07f16819183b2
13Leader.net <pjones1063@gmail.com>
2021-10-22 14:43:44 -0400

    updates

--- app/src/main/java/com/jones/OntarioVaccinePassport/MainActivity.java
+++ app/src/main/java/com/jones/OntarioVaccinePassport/MainActivity.java
@@ -1,108 +1,122 @@
 package com.jones.OntarioVaccinePassport;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.AssetManager;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.FileUtils;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.graphics.PathUtils;
 
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.net.URI;
-import java.util.Properties;
+import java.net.URISyntaxException;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class MainActivity extends AppCompatActivity  {
 
     private static final int PICKFILE_RESULT_CODE = 2;
-    private static String url = "";
-    private static boolean firstime = true;
+    private static String pfd_uri = "";
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loadProps();
-        if (!"".equals(url))
-            loadQR();
-        else
-            doFilePrompt();
-
-        Button b1 = findViewById(R.id.button);
-        b1.setOnClickListener(this);
+        if (!loadQR())
+            saveProps();
     }
 
-    @Override
-    public void onClick(View view) {
-       // if(view.getId() == R.id.button)
-        //    finishAffinity();
-        doFilePrompt();
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
 
-    private void loadQR() {
+    private boolean loadQR() {
+        verifyStoragePermissions(this);
         ImageView im = findViewById(R.id.imageView);
         FileInputStream in = null;
         try {
-            URI uri = new URI(url);
-            in = new FileInputStream (new File(uri.getPath()));
+            in = new FileInputStream (new File(pfd_uri));
+            im.setImageBitmap(BitmapFactory.decodeStream(in));
+            return true;
         } catch (Exception e) {
             Log.e("loadQR", "loadQR: ",e );
+            return false;
         }
-        im.setImageBitmap(BitmapFactory.decodeStream(in));
     }
 
 
     private void doFilePrompt () {
-        Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
-        chooseFile.setType("*/*");
-        chooseFile = Intent.createChooser(chooseFile, "Choose a png file");
-        startActivityForResult(chooseFile, PICKFILE_RESULT_CODE);
-    }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
-        super.onActivityResult(requestCode, resultCode, resultData);
-        if (requestCode == PICKFILE_RESULT_CODE
-                && resultCode == Activity.RESULT_OK) {
-            Uri uri = null;
-            if (resultData != null) {
-                uri = resultData.getData();
-                url = uri.getPath();
-                if(!"".equals(url))
-                    saveProps();
-            }
-        }
     }
 
     public void loadProps()  {
         SharedPreferences settings = getSharedPreferences("com.jones.OntarioVaccinePassport", Activity.MODE_PRIVATE);
-        url =  settings.getString("com.jones.OntarioVaccinePassport.pdf", "");
+         pfd_uri =  settings.getString("com.jones.OntarioVaccinePassport.pdf", "");
     }
 
 
     public void saveProps()  {
-
         try {
+            pfd_uri = "/storage/emulated/0/Download/qr.png";
             SharedPreferences settings = getSharedPreferences("com.jones.OntarioVaccinePassport", Activity.MODE_PRIVATE);
             SharedPreferences.Editor editor = settings.edit();
-            editor.putString("com.jones.OntarioVaccinePassport.pdf", url);
+            editor.putString("com.jones.OntarioVaccinePassport.pdf", pfd_uri);
             editor.commit();
             loadQR();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+
+    
 }
